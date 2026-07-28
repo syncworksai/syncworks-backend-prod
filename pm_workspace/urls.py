365aa23e84b974ef03ee3ce15cfa8f9ea77b3bb2
syncworksai <syncworks.ai@gmail.com>
2026-07-28 12:40:07 -0500

    Route standalone PM properties

--- pm_workspace/urls.py
+++ pm_workspace/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PMTenantInvitationViewSet, PMTenantViewSet, PMWorkspaceViewSet
+from .views import PMPropertyViewSet, PMTenantInvitationViewSet, PMTenantViewSet, PMWorkspaceViewSet
 
 router = DefaultRouter()
 router.register(r"workspaces", PMWorkspaceViewSet, basename="pm-workspaces")
+router.register(r"properties", PMPropertyViewSet, basename="pm-properties")
 router.register(r"tenants", PMTenantViewSet, basename="pm-tenants")
 router.register(r"tenant-invitations", PMTenantInvitationViewSet, basename="pm-tenant-invitations")
 
