3531b6e31e9e68cd443c63e48d987f59c7f79fc1
syncworksai <syncworks.ai@gmail.com>
2026-07-29 13:55:06 -0500

    Register PM work order model

--- pm_workspace/apps.py
+++ pm_workspace/apps.py
@@ -5,3 +5,6 @@ class PMWorkspaceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pm_workspace"
     verbose_name = "Property Management Workspaces"
+
+    def ready(self):
+        from . import workorder_models  # noqa: F401
