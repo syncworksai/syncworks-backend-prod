97952ce5150a18abd2b646ebffd1cfbbe4cbff1c
syncworksai <syncworks.ai@gmail.com>
2026-08-11 17:02:04 -0500

    Configure SyncWorks Social app

---
+++ platform_social/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PlatformSocialConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "platform_social"
+    verbose_name = "SyncWorks Social"
