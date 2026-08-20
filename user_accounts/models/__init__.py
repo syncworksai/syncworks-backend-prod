148b2e276b47a3b4f6905c6e920ce9445cbf531d
syncworksai <syncworks.ai@gmail.com>
2026-08-20 12:05:02 -0500

    Build 13 export scheduling provider/resource models

--- user_accounts/models/__init__.py
+++ user_accounts/models/__init__.py
@@ -20,7 +20,7 @@ from .audit import AuditLog
 from .notifications import Notification, PlatformNewsItem
 from .categories import ServiceCategory
 from .business import Business, BusinessMember, BusinessCategory, BusinessMemberRole
-from .professional_services import ProfessionalPracticeProfile, ProfessionalAppointment
+from .professional_services import ProfessionalPracticeProfile, ProfessionalProvider, ProfessionalResource, ProfessionalAppointment
 from .service_catalog import ServiceCatalogItem
 from .business_customers import BusinessCustomer
 from .data_imports import BusinessDataImport
@@ -105,7 +105,7 @@ __all__ = [
     "User", "EmailVerificationChallenge", "CustomerProfile", "SmallBusinessOwnerProfile", "SubcontractorProfile",
     "PersonalIdentity", "UserLocation", "BusinessVerification",
     "AuditLog", "Notification", "PlatformNewsItem", "ServiceCategory", "BusinessCategory", "Business",
-    "BusinessMember", "BusinessMemberRole", "ProfessionalPracticeProfile", "ProfessionalAppointment",
+    "BusinessMember", "BusinessMemberRole", "ProfessionalPracticeProfile", "ProfessionalProvider", "ProfessionalResource", "ProfessionalAppointment",
     "ServiceCatalogItem", "BusinessCustomer", "BusinessDataImport",
     "BusinessProject", "BusinessPartnerRelationship", "PartnerWorkTicket", "PartnerWorkChangeOrder",
     "PartnerPaymentAllocation", "PartnerPayment", "PartnerInvoice", "PartnerWorkEstimate", "BusinessPartnerInvitation",
