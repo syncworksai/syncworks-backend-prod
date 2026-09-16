d329b2772b746cdf98ac9bd10b9a2458ceec9ecb
syncworksai <syncworks.ai@gmail.com>
2026-09-16 14:11:35 -0500

    Add sports app config

---
+++ platform_sports/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PlatformSportsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "platform_sports"
+    verbose_name = "SyncWorks Sports"
