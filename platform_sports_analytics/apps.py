5dfe434093b5b5ba4ad5230427024796584c485a
syncworksai <syncworks.ai@gmail.com>
2026-09-16 14:52:49 -0500

    Add advanced softball analytics and GameCast

---
+++ platform_sports_analytics/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PlatformSportsAnalyticsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "platform_sports_analytics"
+    verbose_name = "SyncWorks Sports Analytics"
